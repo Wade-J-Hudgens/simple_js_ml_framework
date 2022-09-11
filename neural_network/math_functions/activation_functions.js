b376994a7c7d45fdf01ccc03e7f7346ca4db19a0
Wade-J-Hudgens <71951530+Wade-J-Hudgens@users.noreply.github.com>
2022-09-10 18:52:17 -0700

    Completed activation functions

--- neural_network/math_functions/activation_functions.js
+++ neural_network/math_functions/activation_functions.js
@@ -4,7 +4,6 @@ class ActivationFunctions {
         sigmoid: 3,
         linear: 4,
         relu: 5,
-        softmax: 7,
         softplus: 8,
         softsign: 9,
         swish: 10,
@@ -47,6 +46,35 @@ class ActivationFunctions {
         return z<=0?0.01:z;
     }
 
+    static Softplus(z, prime=false) {
+        if (prime) {
+            return (Math.exp(z)/(1+Math.exp(z)));
+        }
+        return Math.log(1 + Math.exp(z));
+    }
+
+    static Softsign(z, prime=false) {
+        if (prime) {
+            return 1/Math.pow(1 + Math.abs(z), 2);
+        }
+        return z / (1 + Math.abs(z));
+    }
+
+    static Swish(z, prime=false) {
+        if (prime) {
+            return ActivationFunctions.Swish(z) * ActivationFunctions.Sigmoid(z) * (1- ActivationFunctions.Swish(z));
+        }
+
+        return z * ActivationFunctions.Sigmoid(z);
+    }
+
+    static tanh(z, prime=false) {
+        if (prime) {
+            return 4 / Math.pow(Math.exp(z) + Math.exp(-z), 2);
+        }
+        return (Math.exp(z) - Math.exp(-z))/(Math.exp(z) + Math.exp(-z))
+    }
+
     static Activation(activation_function, z, prime=false) {
         switch(activation_function) {
             case ActivationFunctions.Activations.relu:
@@ -55,6 +83,18 @@ class ActivationFunctions {
                 return ActivationFunctions.LRelu(z, prime);
             case ActivationFunctions.Activations.linear:
                 return ActivationFunctions.Linear(z, prime);
+            case ActivationFunctions.Activation.elu:
+                return ActivationFunctions.Elu(z, prime);
+            case ActivationFunctions.Activation.elu:
+                return ActivationFunctions.elu(z, prime);
+            case ActivationFunctions.Activation.softplus:
+                return ActivationFunctions.Softplus(z, prime);
+            case ActivationFunctions.Activation.softsign:
+                return ActivationFunctions.Softsign(z, prime);
+            case ActivationFunctions.Activation.swish:
+                return ActivationFunctions.Swish(z, prime);
+            case ActivationFunctions.Activation.tanh:
+                return ActivationFunctions.tanh(z, prime);
         }
     }
 }
